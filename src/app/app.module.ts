ecbaba9dc88555db189add1cfc459d394c7c3282
rubenpablobm <rubenp.barrazam@gmail.com>
2023-04-16 10:56:01 -0600

    Removes housing-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HousingListComponent } from './housing-list/housing-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HousingListComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
