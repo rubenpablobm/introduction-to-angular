88936d8efd503bbc0c57f3901f915e361ddc3521
rubenpablobm <rubenp.barrazam@gmail.com>
2023-04-17 08:14:43 -0600

    Adds event listener to input button

--- src/app/housing-list/housing-list.component.ts
+++ src/app/housing-list/housing-list.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-housing-list',
-  templateUrl: './housing-list.component.html',
-  styleUrls: ['./housing-list.component.css']
+  selector: 'app-housing-list',                 // tag name
+  templateUrl: './housing-list.component.html', // template location
+  styleUrls: ['./housing-list.component.css']   // styles location
 })
 export class HousingListComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  searchHousingLocations(searchText: string) {
+    console.log(searchText);
   }
 
 }
